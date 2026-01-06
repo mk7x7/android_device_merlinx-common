681dcf2038192726fbf92ffbe6133a5d6703c9a7
mk7x7 <mikxelsilvax777@gmail.com>
2026-01-06 00:18:15 -0300

    mt6768-common: move to speed-profile

--- mt6768.mk
+++ mt6768.mk
@@ -421,8 +421,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# Everything profile to better responsive and perf
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
