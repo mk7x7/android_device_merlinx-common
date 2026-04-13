cb4c1c27669e556b17d38b25fd88e9864df89d28
mk7x7 <mikxelsilvax777@gmail.com>
2026-04-13 20:09:35 -0300

    mt6768-common: dexpreopt: switch default compiler filter to verify

--- mt6768.mk
+++ mt6768.mk
@@ -452,6 +452,8 @@ PRODUCT_COPY_FILES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
