2e304368b31b71cc46ed92409038604476d01c3f
mk7x7 <mikxelsilvax777@gmail.com>
2026-05-07 18:07:00 -0300

    mt6768-common: Dex2oat: PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER: use speed-profile

--- mt6768.mk
+++ mt6768.mk
@@ -454,7 +454,7 @@ PRODUCT_COPY_FILES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
 
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
