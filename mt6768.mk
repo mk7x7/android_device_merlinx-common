0f5eb1dc9971dc0d4aa7b031360fde442727950a
mk7x7 <mikxelsilvax777@gmail.com>
2026-05-30 13:38:46 -0300

    mt6768-common: inherit custom fonts/icons from github

--- mt6768.mk
+++ mt6768.mk
@@ -492,3 +492,6 @@ $(call inherit-product, vendor/xiaomi/mt6768-common/mt6768-common-vendor.mk)
 
 # ViperFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
+# Addons
+$(call inherit-product, vendor/addons/addons.mk)
